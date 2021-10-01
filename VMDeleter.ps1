05aeae571a5cc78889e325480ae9f5605373edc3
Boolean <jamesglennewton@gmail.com>
2021-10-01 12:31:00 +0100

    Add files via upload

---
+++ VMDeleter.ps1
@@ -0,0 +1,14 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $VMResourceGroupName
+)
+#Connect to Azure
+Connect-AzAccount
+#Delete all VMs in a given resource group
+foreach ($resource_group in Get-AzResourceGroup) {
+    if ($resource_group -eq $VMResourceGroupName) {
+        Get-AzVM -ResourceGroupName $VMResourceGroupName | Stop-AzVM -ResourceGroupName $VMResourceGroupName | Remove-AzVm -ResourceGroupName $VMResourceGroupName
+    }
+}
